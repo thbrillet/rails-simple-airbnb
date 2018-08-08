31f2910c28454892fd5e19fed9960265ad42763a
Thibault Brillet <thbrillet@gmail.com>
2018-08-08 16:00:19 +0200

    CRUD flats ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+Flat.create(name: "Appartement 1", address: "44000 Nantes", description: "Ceci est la descritpion de l'appartement 1", price_per_night: 99, number_of_guests: 3)
+Flat.create(name: "Appartement 2", address: "44000 Nantes", description: "Ceci est la descritpion de l'appartement 2", price_per_night: 99, number_of_guests: 3)
+Flat.create(name: "Appartement 3", address: "44000 Nantes", description: "Ceci est la descritpion de l'appartement 3", price_per_night: 99, number_of_guests: 3)
+Flat.create(name: "Appartement 4", address: "44000 Nantes", description: "Ceci est la descritpion de l'appartement 4", price_per_night: 99, number_of_guests: 3)
+Flat.create(name: "Appartement 5", address: "44000 Nantes", description: "Ceci est la descritpion de l'appartement 5", price_per_night: 99, number_of_guests: 3)
+Flat.create(name: "Appartement 6", address: "44000 Nantes", description: "Ceci est la descritpion de l'appartement 6", price_per_night: 99, number_of_guests: 3)
+Flat.create(name: "Appartement 7", address: "44000 Nantes", description: "Ceci est la descritpion de l'appartement 7", price_per_night: 99, number_of_guests: 3)
+Flat.create(name: "Appartement 8", address: "44000 Nantes", description: "Ceci est la descritpion de l'appartement 8", price_per_night: 99, number_of_guests: 3)
+Flat.create(name: "Appartement 9", address: "44000 Nantes", description: "Ceci est la descritpion de l'appartement 9", price_per_night: 99, number_of_guests: 3)
+Flat.create(name: "Appartement 10", address: "44000 Nantes", description: "Ceci est la descritpion de l'appartement 10", price_per_night: 99, number_of_guests: 3)
+Flat.create(name: "Appartement 11", address: "44000 Nantes", description: "Ceci est la descritpion de l'appartement 11", price_per_night: 99, number_of_guests: 3)
+Flat.create(name: "Appartement 12", address: "44000 Nantes", description: "Ceci est la descritpion de l'appartement 12", price_per_night: 99, number_of_guests: 3)
+Flat.create(name: "Appartement 13", address: "44000 Nantes", description: "Ceci est la descritpion de l'appartement 13", price_per_night: 99, number_of_guests: 3)
+Flat.create(name: "Appartement 14", address: "44000 Nantes", description: "Ceci est la descritpion de l'appartement 14", price_per_night: 99, number_of_guests: 3)
+Flat.create(name: "Appartement 15", address: "44000 Nantes", description: "Ceci est la descritpion de l'appartement 15", price_per_night: 99, number_of_guests: 3)
